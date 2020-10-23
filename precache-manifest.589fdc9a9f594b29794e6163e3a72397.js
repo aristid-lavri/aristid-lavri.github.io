dfcd9e4314260c41ea3760107461ff8efd3f3cec
aristide-lavri <lavri.alain@outlook.com>
2020-10-23 16:56:26 +0000

    Updates

---
+++ precache-manifest.589fdc9a9f594b29794e6163e3a72397.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c0fc9f14f92da52996f99baaeeaec48",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "464d449406cd8b79e234",
+    "url": "/portfolio/static/css/main.f135aca2.chunk.css"
+  },
+  {
+    "revision": "6611198450c41a562167",
+    "url": "/portfolio/static/js/2.d92d3896.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.d92d3896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "464d449406cd8b79e234",
+    "url": "/portfolio/static/js/main.cf522fbb.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4fbdc4a29cbc77839833c52fbc59ae9b",
+    "url": "/portfolio/static/media/luffy.4fbdc4a2.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a4b53b0623898505f58800a054c2a87f.js"
+  "/portfolio/precache-manifest.589fdc9a9f594b29794e6163e3a72397.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
