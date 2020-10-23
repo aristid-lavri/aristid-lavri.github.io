f8efd96fe490a2d43be52304c15a7bb9851342e0
aristide-lavri <lavri.alain@outlook.com>
2020-10-23 16:30:26 +0000

    Updates

---
+++ precache-manifest.4817347c244d36071d27850a9bc87ff6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d275af9a4266e04d5a40d960ed3e614",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c0fcefb682027fb62b7f",
+    "url": "/portfolio/static/css/main.f135aca2.chunk.css"
+  },
+  {
+    "revision": "6611198450c41a562167",
+    "url": "/portfolio/static/js/2.d92d3896.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.d92d3896.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0fcefb682027fb62b7f",
+    "url": "/portfolio/static/js/main.cfee91f7.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "4fbdc4a29cbc77839833c52fbc59ae9b",
+    "url": "/portfolio/static/media/luffy.4fbdc4a2.png"
+  }
+]);
